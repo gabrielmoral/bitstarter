0b107a50535f2a4ba08181b896e6dda13420c794
Gabrielmoral <gabrielmoralortiz@gmail.com>
2013-07-05 17:15:49 +0000

    Added index.html, web.js read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fileContent = fs.readFileSync('index.html');
+    response.send(fileContent.toString());
 });
 
 var port = process.env.PORT || 5000;
